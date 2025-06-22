ce8439a5ea45c943741c85770f9422a9f96b067d
oksanafedorova07 <kcenka4@gmail.com>
2025-06-22 14:01:56 +0300

    correct fail

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import LessonListCreateView, LessonDetailView
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, LessonListCreateView, LessonDetailView
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+
 
 urlpatterns = [
     path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
     path('lessons/<int:pk>/', LessonDetailView.as_view(), name='lesson-detail'),
-]
+] + router.urls
